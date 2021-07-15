3442123b698f510b1f5da8df19096978611ae068
BIbarr <66765878+BIbarr@users.noreply.github.com>
2021-07-15 17:34:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("Hola Mundo");
 console.log("Se le han agregado nuevas características al documento index.js");
+/*------------------ Modificaciones -------------------------------*/
+console.log("Ahora se agregarán modificaciones en el documento");
