b834b60624af3a5b8b46d9bf3b9a32f2230d4abb
Brian Ibarra <briani47@hotmail.com>
2021-07-23 18:04:54 +0000

    cambios realizados desde el entorno theia lab

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("Hola Mundo");
 console.log("Se le han agregado nuevas características al documento index.js");
 /*------------------ Modificaciones -------------------------------*/
 console.log("Ahora se agregarán modificaciones en el documento");
+console.log("Se agregan más modificaciones");
